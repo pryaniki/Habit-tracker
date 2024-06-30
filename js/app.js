d2391b69369a51bcb3ec80083107e5fbd64641e0
Maksim Skoropistsev <hp1183546@gmail.com>
2024-06-30 17:02:45 +0300

    Загрузка и сохранение данных приложения

--- js/app.js
+++ js/app.js
@@ -1,37 +1,22 @@
 'use strict';
 
-function submitForm() {
-    const input = document.querySelector('.input').value;
-    if (!input) {
-        return false;
-    }
-    document.querySelector('.panel').innerText = input;
-    document.querySelector('.input').value = '';
-    // document.querySelector('.notification').style.display = 'block';
-    // document.querySelector('.notification').classList.add('notification_active');
-    document.querySelector('.notification').classList.remove('notification_hidden');
-     
-   console.log(document.querySelector('.notification').getAttribute('class'));
-   document.querySelector('.notification').setAttribute('key', 1);
-}
+let habbits = [];
+const HABBIT_KEY = 'HABBIT_KEY';
+ 
+/* utils */
 
-function inputChange(e) {
-    if (e.code === 'Enter' ) {
-        submitForm();
+function loadData() {
+    const habbitString = localStorage.getItem(HABBIT_KEY);
+    const habbitArray = JSON.parse(habbitString);
+    if (Array.isArray(habbitArray)) {
+        habbits = habbitArray;
     }
 }
 
-localStorage.setItem('token', 'fawsd');
-localStorage.setItem('token2', false);
-const token = localStorage.getItem('token');
-const token2 = localStorage.getItem('token2');
+function setData() {
+    localStorage.setItem(HABBIT_KEY, JSON.stringify(habbits));
+}
 
-console.log(token);
-console.log(typeof token); // string
-
-console.log(token2);
-console.log(typeof token2); // string
-localStorage.removeItem('token2');
-localStorage.clear();
-// console.log();
-// console.log();
+(() => {
+    loadData();
+})()
