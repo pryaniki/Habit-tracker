3bd9c5e22beaa229a092ddb14a3f0d80807051f0
Maksim Skoropistsev <hp1183546@gmail.com>
2024-08-20 06:47:32 +0300

    Рендер контента

--- js/app.js
+++ js/app.js
@@ -10,7 +10,8 @@ const page = {
         h1: document.querySelector('.h1'),
         progressPrecent: document.querySelector('.progress__precent'),
         progressCoverBar: document.querySelector('.progress__cover-bar'),
-    }
+    },
+    content: document.querySelector('.list'),
 }
 
 /* utils */
@@ -27,6 +28,10 @@ function setData() {
     localStorage.setItem(HABBIT_KEY, JSON.stringify(habbits));
 }
 
+function removeComment(habbitId, index) {
+    delete localStorage.HABBIT_KEY.habbitId.days.index;
+}
+
 /* render */
 function rerenderMenu(activeHabbit) {
     if (!activeHabbit) {
@@ -35,7 +40,7 @@ function rerenderMenu(activeHabbit) {
 
     for (const habbit of habbits) {
         const existed = document.querySelector(`[menu-habbit-id="${habbit.id}"]`);
-        console.log(existed);
+        // console.log(existed);
         if (!existed) {
             // создание
             const element = document.createElement('button');
@@ -71,11 +76,40 @@ function reranderHead(activeHabbit) {
     page.header.progressPrecent.innerText = `${progress} %`;
 }
 
+function reranderContent(activeHabbit) {
+    if (!activeHabbit) {
+        return;
+    }
+    page.content.innerHTML = '';
+
+    for (const [i, data] of activeHabbit.days.entries()) {
+        const currentDay = i + 1;
+        console.log(`День ${i+1}: ${data.comment}`);
+        const element = document.createElement('div');
+        element.classList.add('habbit');
+        element.setAttribute('day-number', currentDay);
+        element.innerHTML = `<div class="habbit__day">День ${currentDay}</div>
+        <div class="habbit__comment">${data.comment}</div>`;
+
+        const deliteButton = document.createElement('button');
+        deliteButton.classList.add('habbit__delete');
+        deliteButton.innerHTML = `<img src="images/icons/delete.svg" alt="Удалить День ${currentDay}">`;
+        // deliteButton.addEventListener('click', () => 
+        //     removeComment(activeHabbit.id, i)
+        // );
+
+        element.appendChild(deliteButton);
+
+        page.content.appendChild(element);
+    }
+
+}
 
 function rerander(activeHabbitId) {
     const activeHabbit = habbits.find(habbit => habbit.id === activeHabbitId);
     rerenderMenu(activeHabbit);
     reranderHead(activeHabbit);
+    reranderContent(activeHabbit);
  
 }
 
